b5607d7d7155ce3e24cdbb202cb695ccc1cd8524
Sam Meech-Ward <sam@meech-ward.com>
2016-11-11 16:08:04 -0800

    Initial Commit

---
+++ DrawingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingApp
+//
+//  Created by Sam Meech-Ward on 2016-11-11.
+//  Copyright © 2016 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
